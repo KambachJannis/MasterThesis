cd601ec8af099b284c592d1b9aa9cfb433806fbd
KambachJannis <jannis-kambach@gmx.de>
2021-04-08 13:59:30 +0000

    added WTP loss

--- models/losses/wtp_loss.py
+++ models/losses/wtp_loss.py
@@ -22,9 +22,14 @@ def computeLoss(points, probs):
         # get relevant pixels for this item
         item_ids = probs_flat[item['id_list']]
         # init tensor of same size and fill with label value
-        item_label = torch.ones(item_ids.shape, device = item_ids.device) * item['label']
-        # essentially log function
-        loss += item['scale'] * F.binary_cross_entropy(item_ids, item_label, reduction='mean')
+        item_labels = torch.ones(item_ids.shape, device = item_ids.device) * item['label']
+        # essentially log function and output divided by #items (mean reduction)
+        loss += item['scale'] * F.binary_cross_entropy(item_ids, item_labels, reduction='mean')
+    
+    # add objectness loss
+    objectness = objectnessLoss(points, probs)
+    print(f"base loss: {loss} - objectness: {objectness}")
+    loss += objectness
     
     return loss
 
@@ -43,21 +48,39 @@ def getPixelChecklist(points, probs):
     
     if 0 in class_list:
         # pixel with highest chance for background class
-        id_background = pr_flat.argmin() 
+        id_background = pr_flat.argmin() #returns ONE index
         checklist += [{'scale': 1, 'id_list': [id_background], 'label': 0}]
         
     if 1 in class_list:
-        # pixel with highest chance for foreground class
-        id_foreground = pr_flat.argmax()
+        # pixel with highest chance for blob class
+        id_foreground = pr_flat.argmax() # returns ONE index
         checklist += [{'scale': 1, 'id_list': [id_foreground], 'label': 1}]   
 
     ################ POINT LEVEL ######################
     if 1 in class_list:
         # locate point labels
         ids_labels = torch.where(pt_flat==1)[0]
-        checklist += [{'scale': len(ids_labels), 'id_list': ids_labels, 'label': 1}]  
-
+        checklist += [{'scale': len(ids_labels), 'id_list': ids_labels, 'label': 1}]
+        
     ################ OBJECTNESS ######################
-    # TODO
+    
 
     return checklist
+
+def objectnessLoss(probs):
+    objectness = getObjectness()
+    objectness_flat = objectness.view(-1)
+    pr_flat = probs.view(-1)
+    n = len(pr_flat)
+    
+    ids_background = torch.where(pr_flat == 0)[0]
+    ids_foreground = torch.where(pr_flat == 1)[0]
+    
+    score_background = torch.sum(torch.log(objectness_flat[ids_background]))
+    score_foreground = torch.sum(torch.log(1 - objectness_flat[ids_foreground]))
+    score = (score_background + score_foreground) / n
+    
+    return score
+    
+def getObjectness(): #MATLAB ENGINE MISSING
+    pass
