e2ae13e71b7781994c0d984660723e693ac66d4a
KambachJannis <jannis-kambach@gmx.de>
2021-05-17 14:51:56 +0000

    changed unet backbone

--- models/resunet.py
+++ models/resunet.py
@@ -1,80 +1,230 @@
 import torch
 import torch.nn as nn
-from torchvision import models
-
-def convrelu(in_channels, out_channels, kernel, padding):
-    return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
-        nn.ReLU(inplace=True),
-    )
+from torchvision import models, datasets, transforms
+from torch.nn import functional as F
 
 
-class ResNetUNet(nn.Module):
-    def __init__(self, n_class):
-        super().__init__()
+def get_backbone(name, pretrained=True):
 
-        self.base_model = models.resnet18(pretrained=True)
-        self.base_layers = list(self.base_model.children())
+    """ Loading backbone, defining names for skip-connections and encoder output. """
 
-        self.layer0 = nn.Sequential(*self.base_layers[:3]) # size=(N, 64, x.H/2, x.W/2)
-        self.layer0_1x1 = convrelu(64, 64, 1, 0)
-        self.layer1 = nn.Sequential(*self.base_layers[3:5]) # size=(N, 64, x.H/4, x.W/4)
-        self.layer1_1x1 = convrelu(64, 64, 1, 0)
-        self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
-        self.layer2_1x1 = convrelu(128, 128, 1, 0)
-        self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)
-        self.layer3_1x1 = convrelu(256, 256, 1, 0)
-        self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
-        self.layer4_1x1 = convrelu(512, 512, 1, 0)
+    # TODO: More backbones
 
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+    # loading backbone model
+    if name == 'resnet18':
+        backbone = models.resnet18(pretrained=pretrained)
+    elif name == 'resnet34':
+        backbone = models.resnet34(pretrained=pretrained)
+    elif name == 'resnet50':
+        backbone = models.resnet50(pretrained=pretrained)
+    elif name == 'resnet101':
+        backbone = models.resnet101(pretrained=pretrained)
+    elif name == 'resnet152':
+        backbone = models.resnet152(pretrained=pretrained)
+    elif name == 'vgg16':
+        backbone = models.vgg16_bn(pretrained=pretrained).features
+    elif name == 'vgg19':
+        backbone = models.vgg19_bn(pretrained=pretrained).features
+    # elif name == 'inception_v3':
+    #     backbone = models.inception_v3(pretrained=pretrained, aux_logits=False)
+    elif name == 'densenet121':
+        backbone = models.densenet121(pretrained=True).features
+    elif name == 'densenet161':
+        backbone = models.densenet161(pretrained=True).features
+    elif name == 'densenet169':
+        backbone = models.densenet169(pretrained=True).features
+    elif name == 'densenet201':
+        backbone = models.densenet201(pretrained=True).features
+    elif name == 'unet_encoder':
+        from unet_backbone import UnetEncoder
+        backbone = UnetEncoder(3)
+    else:
+        raise NotImplemented('{} backbone model is not implemented so far.'.format(name))
 
-        self.conv_up3 = convrelu(256 + 512, 512, 3, 1)
-        self.conv_up2 = convrelu(128 + 512, 256, 3, 1)
-        self.conv_up1 = convrelu(64 + 256, 256, 3, 1)
-        self.conv_up0 = convrelu(64 + 256, 128, 3, 1)
+    # specifying skip feature and output names
+    if name.startswith('resnet'):
+        feature_names = [None, 'relu', 'layer1', 'layer2', 'layer3']
+        backbone_output = 'layer4'
+    elif name == 'vgg16':
+        # TODO: consider using a 'bridge' for VGG models, there is just a MaxPool between last skip and backbone output
+        feature_names = ['5', '12', '22', '32', '42']
+        backbone_output = '43'
+    elif name == 'vgg19':
+        feature_names = ['5', '12', '25', '38', '51']
+        backbone_output = '52'
+    # elif name == 'inception_v3':
+    #     feature_names = [None, 'Mixed_5d', 'Mixed_6e']
+    #     backbone_output = 'Mixed_7c'
+    elif name.startswith('densenet'):
+        feature_names = [None, 'relu0', 'denseblock1', 'denseblock2', 'denseblock3']
+        backbone_output = 'denseblock4'
+    elif name == 'unet_encoder':
+        feature_names = ['module1', 'module2', 'module3', 'module4']
+        backbone_output = 'module5'
+    else:
+        raise NotImplemented('{} backbone model is not implemented so far.'.format(name))
 
-        self.conv_original_size0 = convrelu(3, 64, 3, 1)
-        self.conv_original_size1 = convrelu(64, 64, 3, 1)
-        self.conv_original_size2 = convrelu(64 + 128, 64, 3, 1)
+    return backbone, feature_names, backbone_output
 
-        self.conv_last = nn.Conv2d(64, n_class, 1)
 
-    def forward(self, input):
-        x_original = self.conv_original_size0(input)
-        x_original = self.conv_original_size1(x_original)
+class UpsampleBlock(nn.Module):
 
-        layer0 = self.layer0(input)
-        layer1 = self.layer1(layer0)
-        layer2 = self.layer2(layer1)
-        layer3 = self.layer3(layer2)
-        layer4 = self.layer4(layer3)
+    # TODO: separate parametric and non-parametric classes?
+    # TODO: skip connection concatenated OR added
 
-        layer4 = self.layer4_1x1(layer4)
-        x = self.upsample(layer4)
-        layer3 = self.layer3_1x1(layer3)
-        x = torch.cat([x, layer3], dim=1)
-        x = self.conv_up3(x)
+    def __init__(self, ch_in, ch_out=None, skip_in=0, use_bn=True, parametric=False):
+        super(UpsampleBlock, self).__init__()
 
-        x = self.upsample(x)
-        layer2 = self.layer2_1x1(layer2)
-        x = torch.cat([x, layer2], dim=1)
-        x = self.conv_up2(x)
+        self.parametric = parametric
+        ch_out = ch_in/2 if ch_out is None else ch_out
 
-        x = self.upsample(x)
-        layer1 = self.layer1_1x1(layer1)
-        x = torch.cat([x, layer1], dim=1)
-        x = self.conv_up1(x)
+        # first convolution: either transposed conv, or conv following the skip connection
+        if parametric:
+            # versions: kernel=4 padding=1, kernel=2 padding=0
+            self.up = nn.ConvTranspose2d(in_channels=ch_in, out_channels=ch_out, kernel_size=(4, 4),
+                                         stride=2, padding=1, output_padding=0, bias=(not use_bn))
+            self.bn1 = nn.BatchNorm2d(ch_out) if use_bn else None
+        else:
+            self.up = None
+            ch_in = ch_in + skip_in
+            self.conv1 = nn.Conv2d(in_channels=ch_in, out_channels=ch_out, kernel_size=(3, 3),
+                                   stride=1, padding=1, bias=(not use_bn))
+            self.bn1 = nn.BatchNorm2d(ch_out) if use_bn else None
 
-        x = self.upsample(x)
-        layer0 = self.layer0_1x1(layer0)
-        x = torch.cat([x, layer0], dim=1)
-        x = self.conv_up0(x)
+        self.relu = nn.ReLU(inplace=True)
 
-        x = self.upsample(x)
-        x = torch.cat([x, x_original], dim=1)
-        x = self.conv_original_size2(x)
+        # second convolution
+        conv2_in = ch_out if not parametric else ch_out + skip_in
+        self.conv2 = nn.Conv2d(in_channels=conv2_in, out_channels=ch_out, kernel_size=(3, 3),
+                               stride=1, padding=1, bias=(not use_bn))
+        self.bn2 = nn.BatchNorm2d(ch_out) if use_bn else None
 
-        out = self.conv_last(x)
+    def forward(self, x, skip_connection=None):
 
-        return out
+        x = self.up(x) if self.parametric else F.interpolate(x, size=None, scale_factor=2, mode='bilinear',
+                                                             align_corners=None)
+        if self.parametric:
+            x = self.bn1(x) if self.bn1 is not None else x
+            x = self.relu(x)
+
+        if skip_connection is not None:
+            x = torch.cat([x, skip_connection], dim=1)
+
+        if not self.parametric:
+            x = self.conv1(x)
+            x = self.bn1(x) if self.bn1 is not None else x
+            x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x) if self.bn2 is not None else x
+        x = self.relu(x)
+
+        return x
+
+
+class Unet(nn.Module):
+
+    """ U-Net (https://arxiv.org/pdf/1505.04597.pdf) implementation with pre-trained torchvision backbones."""
+
+    def __init__(self,
+                 backbone_name='resnet50',
+                 pretrained=True,
+                 encoder_freeze=False,
+                 n_classes=2,
+                 decoder_filters=(256, 128, 64, 32, 16),
+                 parametric_upsampling=False,
+                 shortcut_features='default',
+                 decoder_use_batchnorm=False):
+        super(Unet, self).__init__()
+
+        self.backbone_name = backbone_name
+
+        self.backbone, self.shortcut_features, self.bb_out_name = get_backbone(backbone_name, pretrained=pretrained)
+        shortcut_chs, bb_out_chs = self.infer_skip_channels()
+        if shortcut_features != 'default':
+            self.shortcut_features = shortcut_features
+
+        # build decoder part
+        self.upsample_blocks = nn.ModuleList()
+        decoder_filters = decoder_filters[:len(self.shortcut_features)]  # avoiding having more blocks than skip connections
+        decoder_filters_in = [bb_out_chs] + list(decoder_filters[:-1])
+        num_blocks = len(self.shortcut_features)
+        for i, [filters_in, filters_out] in enumerate(zip(decoder_filters_in, decoder_filters)):
+            print('upsample_blocks[{}] in: {}   out: {}'.format(i, filters_in, filters_out))
+            self.upsample_blocks.append(UpsampleBlock(filters_in, filters_out,
+                                                      skip_in=shortcut_chs[num_blocks-i-1],
+                                                      parametric=parametric_upsampling,
+                                                      use_bn=decoder_use_batchnorm))
+
+        self.final_conv = nn.Conv2d(decoder_filters[-1], n_classes, kernel_size=(1, 1))
+
+        if encoder_freeze:
+            self.freeze_encoder()
+
+        self.replaced_conv1 = False  # for accommodating  inputs with different number of channels later
+
+    def freeze_encoder(self):
+
+        """ Freezing encoder parameters, the newly initialized decoder parameters are remaining trainable. """
+
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+    def forward(self, *input):
+
+        """ Forward propagation in U-Net. """
+
+        x, features = self.forward_backbone(*input)
+
+        for skip_name, upsample_block in zip(self.shortcut_features[::-1], self.upsample_blocks):
+            skip_features = features[skip_name]
+            x = upsample_block(x, skip_features)
+
+        x = self.final_conv(x)
+        return x
+
+    def forward_backbone(self, x):
+
+        """ Forward propagation in backbone encoder network.  """
+
+        features = {None: None} if None in self.shortcut_features else dict()
+        for name, child in self.backbone.named_children():
+            x = child(x)
+            if name in self.shortcut_features:
+                features[name] = x
+            if name == self.bb_out_name:
+                break
+
+        return x, features
+
+    def infer_skip_channels(self):
+
+        """ Getting the number of channels at skip connections and at the output of the encoder. """
+
+        x = torch.zeros(1, 3, 224, 224)
+        has_fullres_features = self.backbone_name.startswith('vgg') or self.backbone_name == 'unet_encoder'
+        channels = [] if has_fullres_features else [0]  # only VGG has features at full resolution
+
+        # forward run in backbone to count channels (dirty solution but works for *any* Module)
+        for name, child in self.backbone.named_children():
+            x = child(x)
+            if name in self.shortcut_features:
+                channels.append(x.shape[1])
+            if name == self.bb_out_name:
+                out_channels = x.shape[1]
+                break
+        return channels, out_channels
+
+    def get_pretrained_parameters(self):
+        for name, param in self.backbone.named_parameters():
+            if not (self.replaced_conv1 and name == 'conv1.weight'):
+                yield param
+
+    def get_random_initialized_parameters(self):
+        pretrained_param_names = set()
+        for name, param in self.backbone.named_parameters():
+            if not (self.replaced_conv1 and name == 'conv1.weight'):
+                pretrained_param_names.add('backbone.{}'.format(name))
+
+        for name, param in self.named_parameters():
+            if name not in pretrained_param_names:
+                yield param
